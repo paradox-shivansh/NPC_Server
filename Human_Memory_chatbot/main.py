4600b0e2b85f7f6ce498768cc7916c30657950b0
paradox-shivansh <theaverageguy19@gmail.com>
2026-09-04 22:54:04 +0530

    main main.py and editiong the file names according the inponding error

---
+++ Human_Memory_chatbot/main.py
@@ -0,0 +1,90 @@
+from memory.entity_memory import EntityMemory
+
+from memory.episodic_memory import EpisodicMemory
+
+from memory.summary_memory import SummaryMemory
+
+from memory.reflection_memory import ReflectionMemory
+
+from memory.charecter_memory import CharacterMemory
+
+from graph.chatbot_graph import build_graph
+
+
+# -----------------------
+# INITIALIZE MEMORIES
+# -----------------------
+
+entity_memory = EntityMemory()
+
+episodic_memory = EpisodicMemory()
+
+summary_memory = SummaryMemory()
+
+reflection_memory = ReflectionMemory()
+
+character_memory = CharacterMemory()
+
+
+# -----------------------
+# BUILD GRAPH
+# -----------------------
+
+chatbot = build_graph(
+
+    entity_memory=entity_memory,
+
+    episodic_memory=episodic_memory,
+
+    summary_memory=summary_memory,
+
+    reflection_memory=reflection_memory,
+
+    character_memory=character_memory
+)
+
+
+# -----------------------
+# CHAT LOOP
+# -----------------------
+
+print("🧠 Human Memory Chatbot Started")
+
+print("Type 'exit' to stop.\n")
+
+
+while True:
+
+    user_message = input("You: ")
+
+
+    if user_message.lower() == "exit":
+
+        print("Goodbye 👋")
+
+        break
+
+
+    result = chatbot.invoke({
+
+        "user_message": user_message,
+
+        "memory_decision": None,
+
+        "entity_context": None,
+
+        "episodic_context": None,
+
+        "summary_context": None,
+
+        "reflection_context": None,
+
+        "character_context": None,
+
+        "response": ""
+    })
+
+
+    print("\nBot:", result["response"])
+
+    print()
